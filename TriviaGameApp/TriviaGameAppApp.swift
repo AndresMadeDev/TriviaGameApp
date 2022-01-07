df9b70f466b1972427f9cdc50de93f0eedd0d032
AndresMadeDev <andres.made.dev@gmail.com>
2022-01-07 16:27:41 -0500

    Initial Commit

---
+++ TriviaGameApp/TriviaGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameAppApp.swift
+//  TriviaGameApp
+//
+//  Created by Andres Made on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
